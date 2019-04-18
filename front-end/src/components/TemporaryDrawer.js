549dd61f5ef196b89d842748db188551bc15fc4a
jlpos <jlpos@192.168.86.136>
2019-04-17 22:59:31 -0500

    Code cleanup.

--- front-end/src/components/TemporaryDrawer.js
+++ front-end/src/components/TemporaryDrawer.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -55,11 +53,11 @@ class TemporaryDrawer extends React.Component {
       <div className={classes.list}>
         <List>
           {['Home', 'Resume', 'Contact Me'].map((text, index) => (
-			<a href={	index == 2 ? "/contact":
-						index == 1 ? "/resume" : "/"}>
+			<a href={	index === 2 ? "/contact":
+						index === 1 ? "/resume" : "/"}>
             <ListItem button key={text} >
-				<ListItemIcon>{	index == 0 ? 	<HomeIcon /> : 
-								index == 1 ? 	<DocIcon /> : 
+				<ListItemIcon>{	index === 0 ? 	<HomeIcon /> : 
+								index === 1 ? 	<DocIcon /> : 
 												<MailIcon />}
 				</ListItemIcon>
 				<ListItemText primary={text} />
