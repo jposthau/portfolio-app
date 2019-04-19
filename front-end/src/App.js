f6a23e09b6f2b9735d4ce430407ffdb4335a8104
jlpos <jlpos@192.168.86.136>
2019-04-19 09:22:00 -0500

    Styling changes.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import TemporaryDrawer from './components/TemporaryDrawer';
+import ButtonBases from './components/ButtonBases'
+import Bio from './components/Bio'
+import { withStyles } from '@material-ui/core/styles';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<TemporaryDrawer/>
+				<div style={{backgroundColor: 'darkGrey', height: '62vh',}}>
+					<Bio/>
+				</div>
+				<ButtonBases/>
 			</div>
 		);
 	}
