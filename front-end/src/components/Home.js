6cfbc28dc0eb0a30c76252d0eed2b9933ed62cca
jlpos <jlpos@192.168.86.136>
2019-04-24 07:41:18 -0500

    Fixed PDF component.

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 import Bio from './Bio'
 import ButtonBases from './ButtonBases'
 
-const styles = theme => ({
-  root: {
-    display: 'flex',
-    maxWidth: '100vw',
-  },
-});
-
-
 class Home extends Component {
+	
 	render() {
 		return (
-			<div className={styles.root}>
+			<div className="root">
 				<Bio/>
 				<ButtonBases/>
 			</div>
